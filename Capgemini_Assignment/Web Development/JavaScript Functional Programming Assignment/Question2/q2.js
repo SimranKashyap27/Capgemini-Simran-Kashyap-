bb880a620dd0e3a8c6ff907ba800dc9762ebec20
SimranKashyap27 <49619085+SimranKashyap27@users.noreply.github.com>
2021-02-27 19:41:54 +0530

    Update q2.js

--- Capgemini_Assignment/Web Development/JavaScript Functional Programming Assignment/Question2/q2.js
+++ Capgemini_Assignment/Web Development/JavaScript Functional Programming Assignment/Question2/q2.js
@@ -1,3 +1,13 @@
+/*Make a function called compose that takes two functions f1 and f2 and returns a new function that,
+when called on a value, will return f1(f2(value)). Assume that f1 and f2 each take exactly one argument.
+var f1 = compose(square, double);
+f1(5); --> 100
+f1(10); --> 400
+var f2 = compose(double, square);
+f2(5); --> 50
+f2(10); --> 200
+*/
+
 const compose = (f1, f2) => (value) => f1(f2(value));
 
 const square = (x) => x * x;
